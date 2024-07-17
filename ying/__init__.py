8525f068c238a29f5b194453483766cddbc88a5f
Yannick Fricke <yannickfricke@googlemail.com>
2024-07-18 01:42:21 +0200

    refactor: refactored the main entrypoint
    It now defines an application which inherits from CliApplication.
    It also uses the "InitCommand" to define the first command.

--- ying/__init__.py
+++ ying/__init__.py
@@ -1,5 +1,40 @@
+import logging
+import sys
+from os import getenv
+
+from rich.logging import RichHandler
+
+from ying.cli import CliApplication
+from ying.commands.InitCommand import InitCommand
+
+
+class Application(CliApplication):
+    def get_name(self):
+        return "ying"
+
+    def get_version(self):
+        return "0.0.0"
+
+    def get_description(self):
+        return None
+
+    def get_commands(self):
+        return [InitCommand()]
+
+
 def main():
-    pass
+    is_debugging = getenv("DEBUG") == "1"
+
+    logging.basicConfig(
+        level=logging.DEBUG if is_debugging else logging.INFO,
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(show_path=False)],
+    )
+    cleaned_program_arguments = sys.argv[1:]
+
+    application = Application()
+    application.run(cleaned_program_arguments)
 
 
 if __name__ == "__main__":
