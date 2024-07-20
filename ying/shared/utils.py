98dd332517f46c6999e2f44831a389d6fb54f2a1
Yannick Fricke <yannickfricke@googlemail.com>
2024-07-20 21:05:07 +0200

    feat(utils): added the "join_parser_parts/1" function
    The function joins an list of strings into a single string.

--- ying/shared/utils.py
+++ ying/shared/utils.py
@@ -8,3 +8,16 @@ def get_ying_version() -> str:
         str: The current version of ying
     """
     return metadata("ying")["Version"]
+
+
+def join_parser_parts(parts: list[str]) -> str:
+    """Joins the given parser parts into a single string
+
+    Args:
+        parts (list[str]): The parts to join
+
+    Returns:
+        str: The joined string
+    """
+
+    return "".join(parts)
