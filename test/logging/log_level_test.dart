f7868ee5e08b3a94dfddcd6d01942791a471914c
Yannick Fricke <yannickfricke@googlemail.com>
2024-09-22 21:52:55 +0200

    test: moved the LogLevel tests into groups

--- test/logging/log_level_test.dart
+++ test/logging/log_level_test.dart
@@ -2,49 +2,47 @@ import 'package:test/test.dart';
 import 'package:ying/logging/log_level.dart';
 
 void main() {
-  test(
-      'getLogLevelFromEnvironment returns the info level when the environment variable is not set',
-      () {
-    var result = getLogLevelFromEnvironment({});
+  group('getLogLevelFromEnvironment', () {
+    test('returns the info level when the environment variable is not set', () {
+      var result = getLogLevelFromEnvironment({});
 
-    expect(result, LogLevel.info);
-  });
+      expect(result, LogLevel.info);
+    });
 
-  test(
-      'getLogLevelFromEnvironment should return the info level when no LogLevel was matched',
-      () {
-    var result = getLogLevelFromEnvironment(
-      {"LOG_LEVEL": "gibberish"},
-    );
-
-    expect(result, LogLevel.info);
-  });
-
-  test('getLogLevelFromEnvironment returns the correctly matched LogLevel', () {
-    var cases = [
-      // uppercased values
-      ("NONE", LogLevel.none),
-      ("ERROR", LogLevel.error),
-      ("WARN", LogLevel.warn),
-      ("INFO", LogLevel.info),
-      ("DEBUG", LogLevel.debug),
-      ("TRACE", LogLevel.trace),
-
-      // Lowercased values
-      ("none", LogLevel.none),
-      ("error", LogLevel.error),
-      ("warn", LogLevel.warn),
-      ("info", LogLevel.info),
-      ("debug", LogLevel.debug),
-      ("trace", LogLevel.trace),
-    ];
-
-    for (var (environmentVariableValue, expectedLogLevel) in cases) {
+    test('return the info level when no LogLevel was matched', () {
       var result = getLogLevelFromEnvironment(
-        {"LOG_LEVEL": environmentVariableValue},
+        {"LOG_LEVEL": "gibberish"},
       );
 
-      expect(result, expectedLogLevel);
-    }
+      expect(result, LogLevel.info);
+    });
+
+    test('return the correctly matched LogLevel', () {
+      var cases = [
+        // uppercased values
+        ("NONE", LogLevel.none),
+        ("ERROR", LogLevel.error),
+        ("WARN", LogLevel.warn),
+        ("INFO", LogLevel.info),
+        ("DEBUG", LogLevel.debug),
+        ("TRACE", LogLevel.trace),
+
+        // Lowercased values
+        ("none", LogLevel.none),
+        ("error", LogLevel.error),
+        ("warn", LogLevel.warn),
+        ("info", LogLevel.info),
+        ("debug", LogLevel.debug),
+        ("trace", LogLevel.trace),
+      ];
+
+      for (var (environmentVariableValue, expectedLogLevel) in cases) {
+        var result = getLogLevelFromEnvironment(
+          {"LOG_LEVEL": environmentVariableValue},
+        );
+
+        expect(result, expectedLogLevel);
+      }
+    });
   });
 }
