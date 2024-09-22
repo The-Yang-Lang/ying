d3bba0a875709baa8a14ed31e647578b57ce7e88
Yannick Fricke <yannickfricke@googlemail.com>
2024-09-22 20:34:11 +0200

    test(stringifier): added tests for enums + class instances

--- test/utils/stringifier_test.dart
+++ test/utils/stringifier_test.dart
@@ -1,4 +1,6 @@
 import 'package:test/test.dart';
+import 'package:ying/logging/ansi_color.dart';
+import 'package:ying/logging/log_level.dart';
 import 'package:ying/utils/stringifier.dart';
 
 void main() {
@@ -14,6 +16,8 @@ void main() {
       ({"key": "value"}, '{\n    "key": "value",\n}'),
       (MapEntry("key", "value"), '"key": "value"'),
       ((1, 2), "(1, 2)"),
+      (LogLevel.info, "LogLevel.info"),
+      (AnsiColor.reset, "AnsiColor()"),
     ];
 
     for (var (input, expected) in cases) {
