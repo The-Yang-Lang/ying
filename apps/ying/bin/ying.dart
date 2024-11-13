c2abc1afa1ebb43f77ae17ee4f43dd641a18c595
YannickFricke <yannickfricke@googlemail.com>
2024-11-13 14:20:47 +0100

    refactor: use the CLI stuff from the "shared" package
    The ying entrypoint now makes use of the CLI stuff which
    is defined in the ying_shared package.

--- apps/ying/bin/ying.dart
+++ apps/ying/bin/ying.dart
@@ -1,3 +1,5 @@
+import 'package:ying/commands/init_command.dart';
+import 'package:ying_shared/cli/application.dart';
 import 'package:ying_shared/logging/logger.dart';
 import 'package:ying_shared/utils/stringify.dart';
 
@@ -7,4 +9,13 @@ void main(List<String> arguments) {
   logger.debug(
     "Got the following program arguments: ${stringify(arguments)}",
   );
+
+  final application = Application.withCommands(
+    name: "ying",
+    description: "The official CLI for the Yang language",
+    "0.0.0",
+    [InitCommand()],
+  );
+
+  application.run(arguments);
 }
